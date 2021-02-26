098c0ba9290ec4be4335d4c05b03abc1bdc77e7f
Antonio Malara <antonio.malara@gmail.com>
2021-02-26 05:03:22 +0100

    Disassembler

---
+++ dostoc/Sources/dostoc/Flow.swift
@@ -0,0 +1,161 @@
+//
+//  Flow.swift
+//  
+//
+//  Created by Antonio Malara on 26/02/21.
+//
+
+import Foundation
+import udis86
+
+enum FlowType {
+    case seq
+    case invd
+    case call
+    case jmp
+    case jcc
+    case ret
+    case hlt
+    
+    init(for instruction: Instruction) {
+        let invalid: [ud_mnemonic_code] = [
+            UD_Iinvd
+        ]
+
+        let call: [ud_mnemonic_code] = [
+            UD_Isyscall,
+            UD_Icall,
+            UD_Ivmcall,
+            UD_Ivmmcall,
+        ]
+
+        let ret: [ud_mnemonic_code] = [
+            UD_Isysret,
+            UD_Iiretw,
+            UD_Iiretd,
+            UD_Iiretq,
+            UD_Iret,
+            UD_Iretf,
+        ]
+        
+        let jmp: [ud_mnemonic_code] = [
+            UD_Ijmp,
+        ]
+        
+        let jcc: [ud_mnemonic_code] = [
+            UD_Ijo,
+            UD_Ijno,
+            UD_Ijb,
+            UD_Ijae,
+            UD_Ijz,
+            UD_Ijnz,
+            UD_Ijbe,
+            UD_Ija,
+            UD_Ijs,
+            UD_Ijns,
+            UD_Ijp,
+            UD_Ijnp,
+            UD_Ijl,
+            UD_Ijge,
+            UD_Ijle,
+            UD_Ijg,
+            UD_Ijcxz,
+            UD_Ijecxz,
+            UD_Ijrcxz,
+            UD_Iloopne,
+            UD_Iloope,
+            UD_Iloop,
+        ]
+        
+        let hlt: [ud_mnemonic_code] = [
+            UD_Ihlt,
+        ]
+
+        if invalid.contains(instruction.mnemonic) {
+            self = .invd
+        }
+        else if call.contains(instruction.mnemonic) {
+            self = .call
+        }
+        else if ret.contains(instruction.mnemonic) {
+            self = .ret
+        }
+        else if jmp.contains(instruction.mnemonic) {
+            self = .jmp
+        }
+        else if jcc.contains(instruction.mnemonic) {
+            self = .jcc
+        }
+        else if hlt.contains(instruction.mnemonic) {
+            self = .hlt
+        }
+        else {
+            self = .seq
+        }
+    }
+}
+
+enum InstructionBranches {
+    case none
+    case jmp(target: UInt64)
+    case jcc(next: UInt64, target: UInt64)
+    case call(next: UInt64, target: UInt64)
+    case seq(next: UInt64)
+}
+
+extension Instruction {
+    var branches: InstructionBranches {
+        let next_target = { () -> UInt64 in
+            
+            switch operands.0.type {
+            case UD_OP_JIMM: fallthrough
+            case UD_OP_IMM:
+                switch operands.0.size {
+                case 8:
+                    let val = operands.0.lval.sbyte
+                    
+                    if val > 0 {
+                        return pc + UInt64(val)
+                    }
+                    else {
+                        return pc - UInt64(-val)
+                    }
+
+                case 16:
+                    let val = operands.0.lval.sword
+                    
+                    if val > 0 {
+                        return pc + UInt64(val)
+                    }
+                    else {
+                        return pc - UInt64(-val)
+                    }
+                    
+                default:
+                    assertionFailure()
+                }
+                
+            case UD_OP_PTR:
+                return UInt64(operands.0.lval.ptr.seg << 4) + UInt64(operands.0.lval.ptr.off)
+                
+            case UD_OP_MEM:
+                assertionFailure("mem")
+                
+            default:
+                assertionFailure("AIEE")
+            }
+            
+            return 0
+        }
+        
+        switch FlowType(for: self) {
+        case .seq:  return .seq  (next: pc)
+        case .invd: return .none
+        case .call: return .call (next: pc, target: next_target())
+        case .jmp:  return .jmp  (          target: next_target())
+        case .jcc:  return .jcc  (next: pc, target: next_target())
+        case .ret:  return .none
+        case .hlt:  return .none
+        }
+    }
+}
---
+++ dostoc/Sources/dostoc/UDis.swift
@@ -0,0 +1,80 @@
+//
+//  UDis.swift
+//  
+//
+//  Created by Antonio Malara on 26/02/21.
+//
+
+import Foundation
+import udis86
+
+struct Instruction {
+    let pc: UInt64
+    let mnemonic: ud_mnemonic_code
+    let operands: (ud_operand, ud_operand, ud_operand, ud_operand)
+    
+    let offset: UInt64
+    let asm: String
+}
+
+class UDis86 {
+    var ud: ud_t
+    let baseAddress: UInt64
+    var userData: UserData
+
+    class UserData {
+        let data: Data
+        var current: UInt64 = 0
+        
+        init(data: Data) {
+            self.data = data
+        }
+        
+        func inputHook() -> Int32 {
+            if current == data.count {
+                return UD_EOI
+            }
+            
+            let input = data[Int(current)]
+            current += 1
+            
+            return Int32(input)
+        }
+    }
+    
+    init(data: Data, base: UInt64 = 0) {
+        ud = ud_t()
+        baseAddress = base
+        userData = UserData(data: data)
+        
+        ud_init(&ud)
+        ud_set_syntax(&ud, ud_translate_intel)
+        ud_set_user_opaque_data(&ud, &userData)
+    }
+    
+    func disassemble(addr: UInt64) -> Instruction? {
+        ud_set_input_hook(&ud) { (ptr) -> Int32 in
+            ud_get_user_opaque_data(ptr)
+                .bindMemory(to: UserData.self, capacity: 1)
+                .pointee
+                .inputHook()
+        }
+        
+        userData.current = addr - baseAddress
+        ud_set_pc(&ud, addr)
+
+        if ud_disassemble(&ud) == 0 {
+            return nil
+        }
+        
+        return Instruction(
+            pc: ud.pc,
+            mnemonic: ud_insn_mnemonic(&ud),
+            operands: ud.operand,
+            offset: ud_insn_off(&ud),
+            asm: String(cString: ud_insn_asm(&ud), encoding: .utf8)!
+        )
+    }
+    
+}
+
